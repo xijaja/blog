a665bb02feae547820a062921c28f99978e4d581
希嘉嘉 <hi@xijaja.com>
2024-04-04 20:19:13 +0800

    Add Google Analytics and optimize logo rendering

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,4 +4,5 @@ export const config = {
   description: "Product Manager by day, programmer by night.",
   keywords:
     "产品经理, 编程, 程序员, 开发, 设计, 用户体验, 产品需求, 产品规划, 项目管理, 敏捷开发, Product Manager, Programming, Programmer, Development, Design, User Experience, Product Requirements, Product Planning, Project Management, Agile Development",
+  google_analytics_id: "G-DEXWQL5S4R",
 };
