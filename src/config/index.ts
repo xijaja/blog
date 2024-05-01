dfe173ecee03b6e2a7d478496e0ba32b5a88288f
希嘉嘉 <hi@xijaja.com>
2024-05-01 19:58:00 +0800

    Update keywords in config/index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,6 @@ export const config = {
   title: "xiwu | ✨ PMer & Coder ✨ - xiwu.me",
   base_title: "✨ PMer & Coder ✨ - xiwu.me",
   description: "Product Manager by day, programmer by night.",
-  keywords:
-    "产品经理, 编程, 程序员, 开发, 设计, 用户体验, 产品需求, 产品规划, 项目管理, 敏捷开发, Product Manager, Programming, Programmer, Development, Design, User Experience, Product Requirements, Product Planning, Project Management, Agile Development",
+  keywords: "xiwu, 習武, PM, Product Manager, Coder, Developer,",
   google_analytics_id: "G-DEXWQL5S4R",
 };
